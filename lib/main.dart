e14dd2dc33581b44ead7f5a1aa85db29f71f2806
Princekumar <princekumar68189@gmail.com>
2025-04-18 23:20:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello World'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          )
+      ),
+    );
+  }
+}
